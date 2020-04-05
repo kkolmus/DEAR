89749cc52e2a436729beb01cc542f72f2cfbff5c
Krzysztof Kolmus <9599107+kkolmus@users.noreply.github.com>
2020-04-05 17:45:18 +0200

    Modified reading in data Added VP tab

--- DEAR/app.R
+++ DEAR/app.R
@@ -1,5 +1,7 @@
 library(shiny)
 library(readxl)
+library(dplyr)
+library(ggplot2)
 
 # Define UI for data upload app ----
 ui <- fluidPage(
@@ -19,20 +21,11 @@ ui <- fluidPage(
                 multiple = FALSE,
                 accept = c(".xlsx", ".xls")),
       
-      # Horizontal line ----
-      tags$hr(),
-      
-      # Input: Select number of rows to display ----
-      radioButtons(inputId = "disp", 
-                   label = "Display",
-                   choices = c(Head = "head",
-                               All = "all"),
-                   selected = "head"),
+      actionButton(inputId = "contents", 
+                   label = "Read in data"),
       
       # Horizontal line ----
       tags$hr(),
-      
-      actionButton("choice", "incorporate external information"),
       
       # Input: Selector for choosing FC value from the uploaded dataset ----
       selectInput(inputId = "GeneID",
@@ -47,43 +40,144 @@ ui <- fluidPage(
       # Input: Selector for choosing  p-values from the uploaded dataset ----
       selectInput(inputId = "p.value",
                   label = "Choose the column with p-value values:",
-                  choices = NULL)
+                  choices = NULL),
+      
+      # Input: Select number of rows to display ----
+      radioButtons(inputId = "disp", 
+                   label = "Arrange data based on:",
+                   choices = c(`Descending p-value` = "desc.pvalue",
+                               `Ascending p-value` = "pvalue",
+                               `Descending fold change` = "desc.fc",
+                               `Ascending fold change` = "fc"),
+                   selected = "desc.pvalue"),
+      
+      actionButton(inputId = "choice2", 
+                   label = "Arrange data"),
+      
+      # Horizontal line ----
+      tags$hr()
+      
     ),
     
     # Main panel for displaying outputs ----
     mainPanel(
       
-      # Output: Data file ----
-      tableOutput("contents")
-      
-      )
+      # Output: Tabset w/ plot and table ----
+      tabsetPanel(type = "tabs",
+                  tabPanel("Content", tableOutput("contents")),
+                  tabPanel("Volcano Plot", plotOutput("volcano_plot")))
+    )
     )
 )
 
 
 server <- function(input, output, session) {
   
-  output$contents <- renderTable({
+  info <- eventReactive(list(input$contents), {
     
     inFile <- req(input$file1)
-
-    df <- read_excel(input$file1$datapath)
+    df <- read_excel(inFile$datapath)
+    df <- as.data.frame(df)
     
-    vars <- names(df)
+    ref <- c()
     
-    if(input$disp == "head") {
-      return(head(df))
-    } else {
-      return(df)
-    }
+    for (i in names(df)){
+      name <- names(df[i])
+      # print(name)
+      # print(class(df[, name]))
+      if (class(df[,i]) == "character") {
+        ref <- c(ref, i) } else {
+          next()
+        }}
     
-    updateSelectInput(session, "GeneID","Choose the column with gene id:", choices = vars)
+    vars1 <- ref
+    vars2 <- grep("FC", names(df), value = TRUE)
+    vars3 <- grep("val", names(df), value = TRUE)
     
-    updateSelectInput(session, "FoldChange","Choose the column with fold change values:", choices = vars)
+    # Update select input immediately after clicking on the action button. 
+    updateSelectInput(session, "GeneID", "Choose the column with gene id:", choices = vars1)
+    updateSelectInput(session, "FoldChange", "Choose the column with fold change values:", choices = vars2)
+    updateSelectInput(session, "p.value", "Choose the column with p-value values:", choices = vars3)
     
-    updateSelectInput(session, "p.value","Choose the column with p-value values:", choices = vars)
+    df
     
   })
+  
+  output$contents <- renderTable({
+    
+    df <- info()
+    
+    df <- select(df, input$GeneID, input$FoldChange, input$p.value)
+    
+    # if (input$disp == "desc.pvalue") {
+    # return(df <- df %>% select(input$GeneID, input$FoldChange, input$p.value) %>%
+    #          arrange(desc(input$p.value)))
+    # } else if (input$disp == "pvalue") {
+    #   return(df <- df %>% select(input$GeneID, input$FoldChange, input$p.value) %>%
+    #            arrange(input$p.value))
+    # } else if (input$disp == "desc.fc") {
+    #   return(df <- df %>% select(input$GeneID, input$FoldChange, input$p.value) %>%
+    #            arrange(desc(input$FoldChange)))
+    # } else {
+    #   return(df <- df %>% select(input$GeneID, input$FoldChange, input$p.value) %>%
+    #            arrange(input$FoldChange))
+    # }
+    
+    # if(input$disp == "desc.pvalue") {
+    # return(arrange_(df, lazyeval::interp(~desc(c), c = as.name(input$p.value))))
+    # } else if(input$disp == "pvalue") {
+    #   return(arrange(df, lazyeval::interp(~c, c = as.name(input$p.value))))
+    # } else if(input$disp == "desc.fc") {
+    #   return(arrange(df, lazyeval::interp(~desc(c), c = as.name(input$FoldChange))))
+    # } else {
+    #   return(arrange(df, lazyeval::interp(~c, c = as.name(input$FoldChange))))
+    # }
+    
+    # if(input$disp == "head") {
+    #   return(head(df))
+    # } else {
+    #   return(df)
+    # }
+    
+    df
+    
+  })
+  
+  output$volcano_plot <- renderPlot({
+    
+    df <- info()
+    
+    df <- select(df, input$GeneID, input$FoldChange, input$p.value)
+    
+    UP = 0.6
+    DOWN = -0.6
+    pval = 0.05
+    
+    df$Threshold <- with(df, ifelse(df[,2] >= UP & df[,3], "Upregulated",
+                                          ifelse(df[,2] <= DOWN & df[,3] < pval,
+                                                 "Downregulated", "Not significant")))
+    
+    vp <- ggplot(data = df,
+                 mapping = aes(x = df[,2],
+                               y = -log10(df[,3]),
+                               color = df[,4])) +
+      geom_point(alpha = 0.3, size = 1) +
+      scale_color_manual(values = c("dodgerblue", "gold", "deeppink2")) +
+      labs(color = "Expression pattern") +
+      xlab("log2FC") +
+      ylab("-log10(p-value)") +
+      theme_bw() +
+      theme(plot.title = element_text(hjust = 0.5),
+            text = element_text(size = 14),
+            legend.text = element_text(size = 14),
+            legend.position = "bottom",
+            axis.text.x = element_text(angle = 0, hjust = 1, size = 14),
+            axis.text.y = element_text(angle = 0, hjust = 1, size = 14))
+    
+    vp
+    
+  })
+  
 }
 
 
